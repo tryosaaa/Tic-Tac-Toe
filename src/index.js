6319949d83af247e41bfe1240e3c6f78d1912232
Mohammad Javad Nikzad <40995737+tryosa@users.noreply.github.com>
2020-05-29 19:22:26 +0430

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from "react"
 import ReactDom, { render } from "react-dom"
 import Game from "./components/Game"
-import {Grid } from '@material-ui/core';
-
-
+import {Grid } from '@material-ui/core'
 import "bootstrap/dist/css/bootstrap.css"
 
 
